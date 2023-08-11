25d59cf133098f5ad79d4a95bc45bfbc9007f126
Hatem Temimi <hatem.temimi@estale.fr>
2023-08-11 17:57:15 +0100

    feat: functional digital watch

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,16 +5,20 @@ function App() {
 
   let [time, setTime] = useState<string>("")
 
+  let [offset, setOffset] = useState<number>(0)
+
+  var today: Date = new Date()
+
   useEffect(()=>{
-    var today: Date 
     setInterval(()=>{
-      today = new Date("france")
+      setOffset(today.getTimezoneOffset())
       setTime(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds())
     }, 1000)
   })
 
   return (
     <div className="overflow-hidden flex items-center  bg-black min-h-screen w-full">
+      <input type="number" onChange={(event)=>setOffset(Number(event.target.value))}></input>
       <div className="flex flex-col w-full items-center">
         <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
           TimeTeller
@@ -22,6 +26,10 @@ function App() {
         <h1 className="text-4xl font-bold tracking-tight text-white sm:text-8xl">
           {time}
         </h1>
+
+        <p className="mt-4 text-xl text-gray-400">
+          Current Offset: {offset}
+        </p>
         <p className="mt-4 text-xl text-gray-400">
           Digital Watch
         </p>
