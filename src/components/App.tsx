fa5b300b2b6534d4000dbbd842d627ed2bbbea33
Hatem Temimi <hatem.temimi@estale.fr>
2023-08-11 17:27:25 +0100

    feat: functional digital watch

---
+++ src/components/App.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+
+
+function App() {
+
+  let [time, setTime] = useState<string>("")
+
+  useEffect(()=>{
+    var today: Date 
+    setInterval(()=>{
+      today = new Date()
+      setTime(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds())
+    }, 1000)
+  })
+
+  return (
+    <div className="overflow-hidden flex items-center  bg-black min-h-screen w-full">
+      <div className="flex flex-col w-full items-center">
+        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          TimeTeller
+        </h1>
+        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-8xl">
+          {time}
+        </h1>
+        <p className="mt-4 text-xl text-gray-400">
+          Digital Watch
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
