de6dc857120591bdf25dd8acaed43d2f38020880
Hatem Temimi <hatem.temimi@estale.fr>
2023-08-11 19:04:21 +0100

    feat: state not stabel

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,32 +3,59 @@ import { useEffect, useState } from "react"
 
 function App() {
 
-  let [time, setTime] = useState<string>("")
+  let [seconds, setSeconds] = useState<number>(0)
 
-  let [offset, setOffset] = useState<number>(0)
+  let [minutes, setMinutes] = useState<number>(0)
+
+  let [hours, setHours] = useState<number>(0)
 
   var today: Date = new Date()
 
+  const init = ()=>{
+    setSeconds(today.getSeconds())
+  }
+
+  const incrementSeconds = ()=>{
+    setSeconds(prev => prev+1)
+  }
+
+  const incrementMinutes = ()=>{
+    setSeconds(0)
+    setMinutes(prev=>prev+1)
+  }
+
+  const tick = () =>{
+    if (seconds!==60){
+      console.log("seconds below 60")
+      console.log("seconds:",seconds)
+      incrementSeconds()
+    } else {
+      console.log("Resetting..")
+      console.log("seconds:",seconds)
+      incrementMinutes()
+    }
+  }
+
   useEffect(()=>{
-    setInterval(()=>{
-      setOffset(today.getTimezoneOffset())
-      setTime(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds())
-    }, 1000)
-  })
+    init()
+    setInterval(()=>tick(),1000)
+
+  },[])
+
+
+
 
   return (
     <div className="overflow-hidden flex items-center  bg-black min-h-screen w-full">
-      <input type="number" onChange={(event)=>setOffset(Number(event.target.value))}></input>
       <div className="flex flex-col w-full items-center">
         <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
           TimeTeller
         </h1>
         <h1 className="text-4xl font-bold tracking-tight text-white sm:text-8xl">
-          {time}
+          0:{minutes}:{seconds}
         </h1>
-
         <p className="mt-4 text-xl text-gray-400">
-          Current Offset: {offset}
+          Current Offset: 0
         </p>
         <p className="mt-4 text-xl text-gray-400">
           Digital Watch
