ab0ca701954eb87ea09f5622940442dd865a8d6a
Hatem Temimi <hatem.temimi@estale.fr>
2023-08-12 13:17:50 +0100

    fix: casio databank integration

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,85 +1,14 @@
 import { useEffect, useState } from "react"
 import useInterval from '../hooks/useInterval'
+import MainDisplay from "./Digital"
+import Casio from "./Casio"
 
 
 function App() {
 
-  var today: Date = new Date()
-
-  var [seconds, setSeconds] = useState<number>(0)
-
-  var [minutes, setMinutes] = useState<number>(0)
-
-  var [hours, setHours] = useState<number>(0)
-
-  const init = ()=>{
-    setSeconds(today.getSeconds())
-
-    setMinutes(today.getMinutes())
-
-    setHours(today.getHours())
-  }
-
-  const incrementSeconds = ()=>{
-    setSeconds(prev => prev+1)
-  }
-
-  const incrementMinutes = ()=>{
-    setSeconds(0)
-    setMinutes(prev=>prev+1)
-  }
-
-
-  const incrementHours = ()=>{
-    setSeconds(0)
-    setMinutes(0)
-    setHours(prev=>prev+1)
-  }
-
-  const tick = () =>{
-    if (seconds!==60){
-    //  console.log("seconds below 60")
-     // console.log("seconds:",seconds)
-      incrementSeconds()
-    } else {
-     // console.log("Resetting..")
-    //  console.log("seconds:",seconds)
-      if (minutes!==60){
-      incrementMinutes()
-      } else {
-        incrementHours()
-      }
-    }
-  }
-
-  useEffect(()=>{
-    init()
-  },[])
-
-    useInterval(()=>{
-      tick()
-    }, 1000, null)
-    // setInterval(()=> {
-    //   tick();
-    //   console.log('seconds:', seconds)
-    // },1000)
-
   return (
-    <div className="overflow-hidden flex items-center  bg-black min-h-screen w-full">
-      <div className="flex flex-col w-full items-center">
-        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-          TimeTeller
-        </h1>
-        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-8xl">
-          {hours}:{minutes}:{seconds}
-        </h1>
-        <p className="mt-4 text-xl text-gray-400">
-          Current Offset: 0
-        </p>
-        <p className="mt-4 text-xl text-gray-400">
-          Digital Watch
-        </p>
-      </div>
+    <div className="flex w-full h-screen bg-black justify-center items-center">
+      <Casio></Casio>
     </div>
   )
 }
---
+++ src/components/Casio.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import MainDisplay from './Digital'
+
+export default function Casio() {
+
+  let buttons: string[] = ["0","1","2"
+                          ,"3","4","5",
+                           "6","7","8",
+                           "9","=","C"]
+
+  let calculator = buttons.map((element)=>{
+    return <button key={element}
+     className="bg-black hover:border-yellow-700 hover:bg-yellow-400 hover:text-black text-white font-bold py-2 px-4
+     border-b-8 border-yellow-400 hover:yellow -blue-500">
+      {element}
+    </button>
+  })
+
+  return (
+      <div className='bg-white w-2/4 h-3/4 rounded'>
+        <div className='flex flex-col divide-y-8 divide-yellow-600 w-full h-full'>
+          <div className='w-auto h-1/2 rounded-md border-8 border-yellow-400'>
+            <div className='bg-black flex flex-row h-full w-full p-4 space-x-4'>
+              <div className='text-yellow-400 flex flex-col justify-evenly'> 
+                <button className='rotate-90'>{"►Adjust"}</button>
+                <button className='rotate-90'>{"►Mode"}</button>
+               </div>
+              <div className='h-full w-full flex-col'>
+                <div className='flex flex-row justify-between'>
+                  <p className='text-yellow-400 font-bold'>CASIO</p>
+                  <p className='text-white font-extrabold'>{"<ILLUMINATOR>"}</p>
+                </div>
+                <div className='bg-yellow-400 h-4/5  w-full flex justify-center items-center'>
+                  <MainDisplay></MainDisplay>
+                </div>
+                <div className='text-sky-400 font-bold flex flex-row justify-evenly mt-2'>
+                  <p>Multilingual</p>
+                  <p>Databank</p>
+                </div>
+              </div>
+              <div className='text-yellow-400 flex flex-col justify-evenly'> 
+                <button className='rotate-90'>{"►AC"}</button>
+                <button className='rotate-90'>{"►Light"}</button>
+               </div>
+            </div>
+          </div>
+          <div className='bg-yellow-300 w-auto h-2/3 p-2'>
+            <div className='bg-black w-auto h-full p-2 rounded-md'>
+              <div className='bg-yellow-400 w-auto h-full p-2'>
+                <div className='bg-black w-auto h-full grid grid-cols-4 divide-x-2 divide-yellow-400'>
+                  {calculator}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
